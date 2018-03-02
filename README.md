eec4157729afbe8c183cb4e0991b05547260ba85
Paul Mayer <paul@paul-mayer.eu>
2018-03-02 14:19:25 +0100

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+#factorio setup
+
+1. Download factorio headless 
+2. go into factorio unpacked folder
+3. change add "password" "username" and "game_password" for public visibility in server-settings.config.json
+4. run
+
+    `
+    bash ./start-standart-server.sh
+    `
---
+++ start-standard-server.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+mkdir saves
+../bin/x64/factorio --create saves/standard-server.zip --map-gen-settings ./map_generator.config.json --map-settings ./map_settings.config.json
+../bin/x64/factorio --start-server-load-latest --server-settings ./server-settings.config.json
