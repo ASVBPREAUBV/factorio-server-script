0a5911e386fad8bcdbbc68e128d2cce3164f6b7e
Paul Mayer <paul@paul-mayer.eu>
2018-03-02 14:35:57 +0100

    fix sh typo

--- start-standard-server.sh
+++ start-standard-server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-rm -rf saves
+rm -rf saves/
 mkdir saves
 ../bin/x64/factorio --create saves/standard-server.zip --map-gen-settings ./map_generator.config.json --map-settings ./map_settings.config.json
 ../bin/x64/factorio --start-server-load-latest --server-settings ./server-settings.config.json
